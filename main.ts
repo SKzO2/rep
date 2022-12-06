08f897dd73f87a2ce9131fd83c7714c098cb016b
SKzO2 <119703021+SKzO2@users.noreply.github.com>
2022-12-06 12:06:25 +0100

    Aktualizacja pxt.json, main.blocks, main.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
-basic.forever(function () {
-	
-})
+namespace GratyTaty {
+    /**
+     *  trying to use some function in Polish
+     * @param x the horizontal coordinate of the LED
+     * @param y the vertical coordinate of the LED
+     */
+    //% blockId=cos %x %y"
+    //% x.min=0 x.max=4 y.min=0 y.max=4
+    //% x.fieldOptions.precision=1 y.fieldOptions.precision=1
+     export function mypoint(x: number, y: number) {
+         led.plot(x,y);
+    }
+
+}
--- test.ts
+++ test.ts
@@ -1,1 +1,2 @@
 // tu są testy; nie będzie to kompilowane, gdy ten pakiet będzie używany jako rozszerzenie.
+GratyTaty.mypoint(1,1);
